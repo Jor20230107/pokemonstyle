363b703d0b9f8f6247070b997a635868134c34f2
kwanghee_cho <bcrisis29@gmail.com>
2024-04-27 10:31:05 +0900

    Unity 프로젝트 생성

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private bool isMoving;
+    private Vector2 input;
+
+    // Start is called before the first frame update
+    
+    private Animator animator;
+
+    private void Awake(){
+        animator = GetComponent<Animator>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if (!isMoving){
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+            // remove diagonal movement
+            if (input.x != 0) input.y = 0;
+
+            if (input != Vector2.zero){
+                animator.SetFloat("MoveX", input.x);
+                animator.SetFloat("MoveY", input.y);
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+                StartCoroutine(Move(targetPos));
+            }
+
+        }
+        animator.SetBool("isMoving",isMoving);
+    }
+
+    IEnumerator Move(Vector3 targetPos){
+        isMoving = true;
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon){
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+        isMoving = false;
+    }
+}
