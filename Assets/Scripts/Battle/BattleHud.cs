02634594f9223664a1d945e889817cd031d924a2
KwangHee Cho <bcrisis29@gmail.com>
2024-04-28 16:16:49 +0900

    implementing attacks in battle system

--- Assets/Scripts/Battle/BattleHud.cs
+++ Assets/Scripts/Battle/BattleHud.cs
@@ -9,9 +9,16 @@ public class BattleHud : MonoBehaviour
     [SerializeField] TextMeshProUGUI levelText;
     [SerializeField] HPBar hpBar;
 
+    Pokemon _pokemon;
+
     public void SetData(Pokemon pokemon){
+        _pokemon = pokemon;
         nameText.text = pokemon.Base.Name;
         levelText.text = "Lvl " + pokemon.Level;
         hpBar.SetHP( (float) pokemon.HP / pokemon.MaxHP);
+    }
+
+    public IEnumerator UpdateHP(){
+        yield return hpBar.SetHPSmooth( (float) _pokemon.HP / _pokemon.MaxHP);
     }
 }
--- Assets/Scripts/Battle/BattleSystem.cs
+++ Assets/Scripts/Battle/BattleSystem.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
+
+public enum BattleState { Start, PlayerAction, PlayerMove, EnemyMove, Busy}
 
 public class BattleSystem : MonoBehaviour
 {
@@ -8,16 +11,141 @@ public class BattleSystem : MonoBehaviour
     [SerializeField] BattleHud playerHud;
     [SerializeField] BattleUnit enemyUnit;
     [SerializeField] BattleHud enemyHud;
+    [SerializeField] BattleDialogBox dialogBox;
 
+    BattleState state;
+    int currrentAction;
+    int currentMove;
     private void Start(){
-        SetupBattle();
+        StartCoroutine(SetupBattle());
  
     }
 
-    public void SetupBattle(){
+    public IEnumerator SetupBattle(){
         playerUnit.Setup();
         playerHud.SetData(playerUnit.Pokemon);
         enemyUnit.Setup();
         enemyHud.SetData(enemyUnit.Pokemon);
+
+        dialogBox.SetMoveNames(playerUnit.Pokemon.Moves);
+
+        yield return dialogBox.TypeDialog($"A wild {enemyUnit.Pokemon.Base.Name} appeared.");
+        yield return new WaitForSeconds(1f);
+
+        PlayerAction();
+    }
+
+    void PlayerAction(){
+        state = BattleState.PlayerAction;
+        StartCoroutine(dialogBox.TypeDialog("Choose an action"));
+        dialogBox.EnableActionSelector(true);
+    }
+
+    void PlayerMove(){
+        state = BattleState.PlayerMove;
+        dialogBox.EnableActionSelector(false);
+        dialogBox.EnableDialogText(false);
+        dialogBox.EnableMoveSelector(true);
+    }
+
+    IEnumerator PerformPlayerMove(){
+        state = BattleState.Busy;
+
+        var move = playerUnit.Pokemon.Moves[currentMove];
+        yield return dialogBox.TypeDialog($"{playerUnit.Pokemon.Base.Name} used {move.Base.Name}");
+        yield return new WaitForSeconds(1f);
+        bool isFainted = enemyUnit.Pokemon.TakeDamage(move, playerUnit.Pokemon);
+        yield return enemyHud.UpdateHP();
+
+        if (isFainted){
+            yield return dialogBox.TypeDialog($"{enemyUnit.Pokemon.Base.Name} fainted");
+        }
+        else{
+            StartCoroutine(EnemyMove());
+        }
+    }
+
+    IEnumerator EnemyMove(){
+        state = BattleState.EnemyMove;
+        var move = enemyUnit.Pokemon.GetRandomMove();
+        yield return dialogBox.TypeDialog($"{enemyUnit.Pokemon.Base.Name} used {move.Base.Name}");
+        yield return new WaitForSeconds(1f);
+        bool isFainted = playerUnit.Pokemon.TakeDamage(move, enemyUnit.Pokemon);
+        yield return playerHud.UpdateHP();
+        if (isFainted){
+            yield return dialogBox.TypeDialog($"{playerUnit.Pokemon.Base.Name} fainted");
+        }
+        else{
+            PlayerAction();
+        }
+
+    }
+
+    private void Update() {
+        if (state == BattleState.PlayerAction){
+            HandleActionSelection();
+        }
+        else if (state == BattleState.PlayerMove){
+            HandleMoveSelection();
+        }
+    }
+
+    void HandleActionSelection(){
+        if (Input.GetKeyDown(KeyCode.DownArrow)){
+            if (currrentAction<1){
+                ++currrentAction;
+            }
+            
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow)){
+            if (currrentAction>0){
+                --currrentAction;
+            }
+        }
+
+        dialogBox.UpdateActionSelection(currrentAction);
+
+        if (Input.GetKeyDown(KeyCode.Z)){
+            if (currrentAction == 0){
+                // Fight
+                PlayerMove();
+            }
+            else if (currrentAction==1){
+            // Run
+        }
+        }
+        
+    }
+
+    void HandleMoveSelection(){
+        
+        if (Input.GetKeyDown(KeyCode.RightArrow)){
+            if (currentMove < playerUnit.Pokemon.Moves.Count-1){
+                ++currentMove;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow)){
+            if (currentMove > 0){
+                --currentMove;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow)){
+            if (currentMove < playerUnit.Pokemon.Moves.Count-2){
+                currentMove += 2;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow)){
+            if (currentMove > 1){
+                currentMove -= 2;
+            }
+        }
+
+        dialogBox.UpdateMoveSelection(currentMove, playerUnit.Pokemon.Moves[currentMove]);
+
+        if (Input.GetKeyDown(KeyCode.Z)){
+            dialogBox.EnableMoveSelector(false);
+            dialogBox.EnableDialogText(true);
+            StartCoroutine(PerformPlayerMove());
+        }
     }
 }
